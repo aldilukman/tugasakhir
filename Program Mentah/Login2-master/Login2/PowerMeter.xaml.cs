6328318c6933fe71f556350ccbc1b7c873461fc4
Aldi <aldilukman007@gmail.com>
2020-06-03 05:26:11 +0700

    update iot

--- Program Mentah/Login2-master/Login2/PowerMeter.xaml.cs
+++ Program Mentah/Login2-master/Login2/PowerMeter.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Net;
 using System.Net.Sockets;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace Login2
 {
@@ -21,131 +23,118 @@ namespace Login2
     /// </summary>
     public partial class PowerMeter : Window
     {
-        private TcpListener listener;
+        ///private TcpListener listener;
         static String controlKWH = "";
         static double harga = 0;
         String daya;
+        MqttClient client;
         public PowerMeter(String nama, String id, String daya)
         {
             InitializeComponent();
-            listener = new TcpListener(IPAddress.Any, 1337);
-            listener.Start();
-            StartAccept();
+            ///listener = new TcpListener(IPAddress.Any, 1337);
+            ///listener.Start();
+            ///StartAccept();
+            client = new MqttClient("broker.hivemq.com");
+            //client.ProtocolVersion = MqttProtocolVersion.Version_3_1_1;
+            string clientId = Guid.NewGuid().ToString();
+            byte code = client.Connect(clientId);
+            if (code == 0)
+            {
+                //Subcribe Topic
+                client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+                client.Subscribe(new string[] { "tugasakhir/status" }, new byte[] { 0 });
+
+            }
+            else
+            {
+
+            }
             Nama.Content = nama;
             IDPelanggan.Content = id;
             Daya.Content = daya;
             this.daya = daya;
         }
-        private void StartAccept()
-        {
-            try
-            {
-                
-                listener.BeginAcceptTcpClient(HandleAsyncConnection, listener);
-                //MessageBox.Show("muncul");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                //MessageBox.Show(e.Message);
-            }
 
-        }
-        private void HandleAsyncConnection(IAsyncResult res)
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
+            //ReceiveAction = Receive;
             try
             {
-                StartAccept();
-                //MessageBox.Show("data");
-                TcpClient clientSocket = listener.EndAcceptTcpClient(res);
-                byte[] bytesFrom = new byte[65555];
-                String dataFromClient;
-                NetworkStream networkStream = clientSocket.GetStream();
-                int lengthh = networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
-                
-                if (networkStream.CanWrite)
-                {
-                    byte[] myWriteBuffer = Encoding.ASCII.GetBytes(controlKWH);
-                    networkStream.Write(myWriteBuffer, 0, myWriteBuffer.Length);
-                    bytesFrom = setNewArray(bytesFrom, lengthh);
-                    controlKWH = "";
-                }
-                
-                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
-                Console.WriteLine(dataFromClient);
-                string[] dataSplit = dataFromClient.Split('_');
+                string message = Encoding.UTF8.GetString(e.Message);
+                MessageBox.Show(message);
+                string[] dataSplit = message.Split('_');
                 Dispatcher.Invoke(() =>
                 {
-                    Voltage.Content = dataSplit[0];
-                    Ampere.Content = dataSplit[1];
-                    Watt.Content = dataSplit[2];
-                    Kwh.Content = dataSplit[3];
-                    Hz.Content = dataSplit[4];
-                    Pf.Content = dataSplit[5];
-                    float hargaDaya = 0;
-                    if (daya.Contains("450 VA"))
+                    try
                     {
-                        hargaDaya = 452;
-                    }else if (daya.Contains("900 VA Subsidi"))
-                    {
-                        hargaDaya = 750;
-                    }
-                    else if (daya.Contains("900 VA Non Subsidi"))
-                    {
-                        hargaDaya = 1352;
-                    }
-                    else if (daya.Contains("1300 VA"))
-                    {
-                        hargaDaya = 1452;
-                    }
-                    float kwhSekarang = float.Parse(dataSplit[3]);
-                    //float kwhSekarang = 1000;
+                        Voltage.Content = dataSplit[0];
+                        Ampere.Content = dataSplit[1];
+                        Watt.Content = dataSplit[2];
+                        Kwh.Content = dataSplit[3];
+                        Hz.Content = dataSplit[4];
+                        Pf.Content = dataSplit[5];
+                        float hargaDaya = 0;
+                        if (daya.Contains("450 VA"))
+                        {
+                            hargaDaya = 452;
+                        }
+                        else if (daya.Contains("900 VA Subsidi"))
+                        {
+                            hargaDaya = 750;
+                        }
+                        else if (daya.Contains("900 VA Non Subsidi"))
+                        {
+                            hargaDaya = 1352;
+                        }
+                        else if (daya.Contains("1300 VA"))
+                        {
+                            hargaDaya = 1452;
+                        }
+                        float kwhSekarang = float.Parse(dataSplit[3]);
+                        //float kwhSekarang = 1000;
 
-                    float hargaSekarang = (hargaDaya / 3600) * kwhSekarang;
-                    //Console.WriteLine(hargaSekarang);
-                    hargaSekarang =hargaSekarang + (hargaSekarang * (60 / 100));
+                        float hargaSekarang = (hargaDaya / 3600) * kwhSekarang;
+                        //Console.WriteLine(hargaSekarang);
+                        hargaSekarang = hargaSekarang + (hargaSekarang * (60 / 100));
+
+                        harga += hargaSekarang;
+                        Rp.Content = harga;
+                    }
+                    catch(Exception err)
+                    {
+                        MessageBox.Show("Format Salah !!!");
+                    }
                     
-                    harga += hargaSekarang;
-                    Rp.Content = harga;
 
 
                 });
-                
-
             }
             catch (Exception ex)
             {
-                //MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.Message);
             }
-        }
 
-        public byte[] setNewArray(byte[] data, int panjangBaru)
-        {
-            byte[] dataBaru = new byte[panjangBaru];
-            for (int i = 0; i < panjangBaru; i++)
-            {
-                dataBaru[i] = data[i];
-            }
-            return dataBaru;
         }
 
         private void Logout(object sender, RoutedEventArgs e)
         {
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
-            listener.Stop();
+            ///listener.Stop();
             this.Close();
         }
 
         private void KWHOFF(object sender, RoutedEventArgs e)
         {
-            controlKWH = "mati";
+            string topic = "tugasakhir/control";
+            ushort ush = client.Publish(topic, Encoding.UTF8.GetBytes("0"), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
         }
-        
+
 
         private void KWHON(object sender, RoutedEventArgs e)
         {
-            controlKWH = "nyala";
+            string topic = "tugasakhir/control";
+            ushort ush = client.Publish(topic, Encoding.UTF8.GetBytes("1"), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
         }
     }
 }
